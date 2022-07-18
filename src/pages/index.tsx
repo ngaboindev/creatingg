592f9fd894288f78d1ffbf8520e4854175429776
ngaboindev <robertngabo2002@gmail.com>
2022-07-18 14:37:09 +0200

    chore: project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@chakra-ui/react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1> <Button>Hello</Button>{' '}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'Creatingg',
+  title: 'Creatingg',
+  description: 'Showcase your portfolio with a website built in minutes.',
+  locale: 'en',
+};
