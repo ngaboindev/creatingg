525c8d9b236058ab593c036035f9cc28c401c6dd
ngaboindev <robertngabo2002@gmail.com>
2022-07-23 16:48:30 +0200

    chore: linted app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+
 import theme from '@/theme';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,17 +1,15 @@
+import * as React from 'react';
 
-import * as React from 'react'
-
-import { Meta } from '@/layouts/Meta'
-import AuthForm from '@/components/AuthForm'
+import AuthForm from '@/components/AuthForm';
+import { Meta } from '@/layouts/Meta';
 
 function Signin(): JSX.Element {
   return (
     <>
-      <Meta title='Sign In | Creatingg' />
+      <Meta title="Sign In | Creatingg" />
       <AuthForm mode="signin" />
     </>
-  )
+  );
 }
 
-
-export default Signin
+export default Signin;
