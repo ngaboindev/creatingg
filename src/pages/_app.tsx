6a304ed3843f35fa304f83b21191c46f92c61e5b
Robert Ngabo <robertngabo2002@gmail.com>
2022-07-28 01:11:50 +0200

    style: added nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import { store } from '@/redux/store';
@@ -7,6 +8,7 @@ import theme from '@/theme';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider theme={theme}>
+    <NextNProgress />
     <ReduxProvider store={store}>
       <Component {...pageProps} />
     </ReduxProvider>
